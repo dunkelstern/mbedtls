2f090940633b867038ad91b91ef7224ccdd027d7
Thomas Fossati <thomas.fossati@nokia.com>
2016-07-17 15:55:45 +0100

    HKDF implementation -- fixed error codes

--- scripts/generate_errors.pl
+++ scripts/generate_errors.pl
@@ -34,7 +34,8 @@ my @low_level_modules = ( "AEAD_CHACHA20_POLY1305",
                           "BASE64", "XTEA", "PBKDF2", "OID", "POLY1305",
                           "PADLOCK", "DES", "NET", "CTR_DRBG", "ENTROPY",
                           "HMAC_DRBG", "MD2", "MD4", "MD5", "RIPEMD160",
-                          "SHA1", "SHA256", "SHA512", "GCM", "THREADING", "CCM" );
+                          "SHA1", "SHA256", "SHA512", "GCM", "THREADING", "CCM",
+                          "HKDF" );
 my @high_level_modules = ( "PEM", "X509", "DHM", "RSA", "ECP", "MD", "CIPHER", "SSL",
                            "PK", "PKCS12", "PKCS5" );
 
